1733e96d667fd1865d10b4974d0931d502238555
Harald Lonmo <lonhar12@nith.no>
2014-01-07 15:18:28 +0100

    tir 15.18

---
+++ Unity prosjekt/Assets/HookScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HookScript : MonoBehaviour
+{
+
+    public Transform IceCubePosition;
+}
---
+++ Unity prosjekt/Assets/TransportScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransportScript : MonoBehaviour
+{
+
+    public Transform StartPos;
+    public Transform EndPos;
+
+    public float Speed;
+
+    public GameObject HookPrefab;
+    public GameObject[] IceCubePrefabs;
+
+    private void Spawn()
+    {
+        var t = (GameObject)Instantiate(HookPrefab, StartPos.position, Quaternion.identity);
+        var c = (GameObject)Instantiate(IceCubePrefabs[Random.Range(0, IceCubePrefabs.Length - 1)], 
+            StartPos.position - new Vector3(0, 2.5f, 0), Quaternion.identity);
+        c.transform.parent = t.GetComponent<HookScript>().transform;
+        c.transform.localPosition = Vector3.zero;
+    }
+}
