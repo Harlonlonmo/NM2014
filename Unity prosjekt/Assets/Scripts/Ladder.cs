f35789e2eb2b6953c48813ca0dca5213bc5051fe
Harald Lonmo <lonhar12@nith.no>
2014-01-08 16:52:44 +0100

    ons 16.52

--- Unity prosjekt/Assets/Scripts/Ladder.cs
+++ Unity prosjekt/Assets/Scripts/Ladder.cs
@@ -1,12 +1,49 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterMotor))]
 public class Ladder : MonoBehaviour
 {
 
-    void Update()
-    {
+    private CharacterController controller;
+    private CharacterMotor motor;
 
+    private float defaultSlopeLimit;
+    private bool defaultSlidingEnabled;
+
+    public float ClimbSlopeLimit = 85;
+    public bool ClimbSlidingEnabled = false;
+
+    public bool active = false;
+    
+    void Start()
+    {
+        controller = transform.GetComponent<CharacterController>();
+        defaultSlopeLimit = controller.slopeLimit;
+        motor = transform.GetComponent<CharacterMotor>();
+        defaultSlidingEnabled = motor.sliding.enabled;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ladder")
+        {
+            controller.slopeLimit = ClimbSlopeLimit;
+            motor.sliding.enabled = ClimbSlidingEnabled;
+            active = true;
+        }
+    } 
+ 
+    void OnTriggerExit (Collider other) 
+    { 
+        if(other.gameObject.tag == "ladder")
+        {
+            controller.slopeLimit = defaultSlopeLimit;
+            motor.sliding.enabled = defaultSlidingEnabled;
+            active = false;
+        }
+    
     }
 }
 
