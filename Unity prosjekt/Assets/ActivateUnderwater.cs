ceb3efa740a574e0e326fac895f3ce3566faaaf9
Matias Snellingen <matias@snellingen.com>
2014-01-08 16:04:12 +0100

    working on submerge

---
+++ Unity prosjekt/Assets/ActivateUnderwater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateUnderwater : MonoBehaviour 
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player") return;
+        other.gameObject.GetComponent<SubmergeHandler>().Submerge();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Player") return;
+        other.gameObject.GetComponent<SubmergeHandler>().Emerge();
+    }
+}
---
+++ Unity prosjekt/Assets/SubmergeHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmergeHandler : MonoBehaviour {
+
+    public Transform underwaterLevel;
+
+    //The scene's default fog settings
+    private bool defaultFog;
+    private Color defaultFogColor;
+    private float defaultFogDensity;
+    private Material defaultSkybox;
+    private Material noSkybox;
+
+    public Color UnderWaterColor = new Color(0, 0.4f, 0.7f, 0.6f);
+    public float UnderWaterFogDensity = 0.04f;
+
+    void Start()
+    {
+        defaultFog = RenderSettings.fog;
+        defaultFogColor = RenderSettings.fogColor;
+        defaultFogDensity = RenderSettings.fogDensity;
+        defaultSkybox = RenderSettings.skybox;
+        //Set the background color
+        camera.backgroundColor = new Color(0, 0.4f, 0.7f, 1);
+    }
+
+	// This method get's called when the object gets submerged in water (enter underwater trigger zone) 
+    public void Submerge()
+    {
+        
+    }
+
+    public void Emerge()
+    {
+        
+    }
+}
