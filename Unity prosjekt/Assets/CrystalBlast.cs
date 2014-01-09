c078212f1a442ba59b69274f32723e2cacebd57f
Matias Snellingen <matias@snellingen.com>
2014-01-09 21:50:44 +0100

    backup push

---
+++ Unity prosjekt/Assets/CrystalBlast.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CrystalBlast : TemperatureEfect {
+    public bool BeamActive;
+    public GameObject[] Beams;
+    public GameObject[] ParticleSystems;
+
+    public LoopingSound Sound;
+
+    public GameObject MuzzleLight;
+    public GameObject ImpactLight;
+
+    public Transform TargetTransform; 
+
+    private List<ParticleSystem> _pSystems;
+
+
+    void Start()
+    {
+        BeamActive = false;
+        foreach (var o in Beams)
+            o.SetActive(false);
+
+        foreach (var system in ParticleSystems)
+        {
+            _pSystems.Add(system.GetComponent<ParticleSystem>());
+            _pSystems.Last().enableEmission = false;
+        }
+
+        MuzzleLight.SetActive(false);
+        ImpactLight.SetActive(false);
+    }
+
+    public override void Activate()
+    {
+        foreach (var beam in Beams)
+        {
+            beam.SetActive(enabled);
+        }
+
+        foreach (ParticleSystem pSystem in _pSystems)
+        {
+            pSystem.enableEmission = enabled;
+        }
+
+
+        MuzzleLight.SetActive(enabled);
+        ImpactLight.SetActive(enabled);
+        if (Sound)
+        {
+            if (enabled)
+            {
+                Sound.Play();
+            }
+            else
+            {
+                Sound.Stop();
+            }
+        }
+    }
+
+    void Update()
+    {
+        SetTarget(transform.position, TargetTransform.position);
+        ImpactLight.transform.position = TargetTransform .position - (TargetTransform.position - transform.position).normalized;
+    }
+
+    public override void Deactivate()
+    {
+
+    }
+
+    void SetTarget(Vector3 pos1, Vector3 pos2)
+    {
+        foreach (var receiver in Beams.SelectMany(beam => beam.GetComponent<LightningEmitter>().lightningReceivers))
+        {
+            receiver.transform.position = pos2;
+        }
+    }
+}
--- Unity prosjekt/Assets/Scripts/Interaction/FreezeHandler.cs
+++ Unity prosjekt/Assets/Scripts/Interaction/FreezeHandler.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class FreezeHandler : HitEfect
 {
     private MeshRenderer _mesh;
+    private bool _melt; 
 
     void Start()
     {
         _mesh = GetComponent<MeshRenderer>();
-        _mesh.enabled = false; 
+        _mesh.enabled = false;
+
         gameObject.layer = 4; 
     }
 
     public override void Activate()
     {
         _mesh.enabled = true;
+        _melt = true; 
         gameObject.layer = LayerMask.NameToLayer("Ice");
         //Debug.Log("frozen");
     }
+
+    void Update()
+    {
+    }
+
 }
