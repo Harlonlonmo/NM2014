2bd67520b1ea18b954b4fd009036e34311bd73b8
Matias Snellingen <matias@snellingen.com>
2014-01-10 22:44:58 +0100

    finish explosion

---
+++ Unity prosjekt/Assets/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Explosion : TemperatureEfect
+{
+
+    public GameObject[] Explosions;
+    public GameObject toDestroy;  
+
+    void Awake()
+    {
+        foreach (var ex in Explosions)
+        {
+            ex.SetActive(false);
+        }
+    }
+    public override void Activate()
+    {
+        foreach (var ex in Explosions)
+        {
+            ex.SetActive(true);
+            if(toDestroy != null)
+                toDestroy.SetActive(false);
+        }
+    }
+
+    public override void Deactivate()
+    {
+        foreach (var ex in Explosions)
+        {
+            ex.SetActive(false);
+        }
+    }
+}
