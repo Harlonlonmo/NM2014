5fedc1deab31c126c34e5b3063eae775f417c198
Matias Snellingen <matias@snellingen.com>
2014-01-10 15:30:01 +0100

    save

---
+++ Unity prosjekt/Assets/Scripts/Trigger/SuctionHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SuctionHandler : MonoBehaviour
+{
+
+    public Transform SuctionPoint;
+    public GameObject Player; 
+    public float SuctionPower;
+    private bool _suck;
+    private CharacterController _characterController; 
+
+    void Start()
+    {
+        _characterController = Player.GetComponent<CharacterController>(); 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        _suck = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _suck = false;
+    }
+
+    void Update()
+    {
+        if (!_suck) return;
+        _characterController.Move((SuctionPoint.position - Player.transform.position).normalized * Time.deltaTime * SuctionPower);
+    }
+
+}
